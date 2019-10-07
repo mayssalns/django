550c078b2a7b1c09eee7365d9a72255d4ee39005
Mayssa Nogueira <mayssa.nogueira@riosoft.systems>
2019-10-07 15:52:42 -0400

    Add Views

--- InternLibrary/core/views.py
+++ InternLibrary/core/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets
+from django_filters import rest_framework as filters
+from .filters import AuthorFilter, BookFilterter
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all().order_by('name')
+    serializer_class = AuthorSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = AuthorFilter
+
+    @action(detail=True)
+    def book(self, request, pk=None):
+        return Response(list(Book.objects.filter(author=pk).values()))
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all().order_by('name')
+    serializer_class = BookSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = BookFilterter
+
+    @action(detail=True)
+    def author(self, request, pk=None):
+
+        return Response(list(Author.objects.filter(book=pk).values()))
